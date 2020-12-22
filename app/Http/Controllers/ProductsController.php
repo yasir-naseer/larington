1761764dcdbf81fbee2a863cfcbab4577eab5422
unknown <yasirnaseer.0@gmail.com>
2020-12-23 02:34:40 +0500

    adding attribute to carrt

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -281,7 +281,7 @@ class ProductsController extends Controller
         $shop = User::find(1);
         $orders = $shop->api()->rest('GET', '/admin/orders.json');
 
-        dd($orders['body']['container']['orders'][0]);
+        dd($orders['body']['container']['orders'][2]);
 
     }
 }
