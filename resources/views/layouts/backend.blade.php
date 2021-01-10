2c51d8a9697d20ba0d29d94d0b5318b4f909ca22
unknown <yasirnaseer.0@gmail.com>
2021-01-10 16:04:20 +0500

    Adding quantity based points in cart

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -221,11 +221,8 @@
             <footer id="page-footer" class="bg-body-light">
                 <div class="content py-3">
                     <div class="row font-size-sm">
-                        <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-right">
-                            Crafted with <i class="fa fa-heart text-danger"></i> by <a class="font-w600" href="https://1.envato.market/ydb" target="_blank">pixelcave</a>
-                        </div>
                         <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-left">
-                            <a class="font-w600" href="https://1.envato.market/AVD6j" target="_blank">OneUI</a> &copy; <span data-toggle="year-copy"></span>
+                            <a class="font-w600" target="_blank" href="https://larington.com/" target="_blank">Larington</a> &copy; <span data-toggle="year-copy"></span>
                         </div>
                     </div>
                 </div>
